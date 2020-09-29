2548de80957d0b3366c41239fe52adbc3ab8ecb8
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-09-29 03:31:14 -0300

    card e conteudo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './components/layout/Card'
+import './App.css'
+export default (props) => (
+    <div className="App">
+        <Card titulo = "Insira abaixo o Link que deseja encurtar">
+            Encurta Link
+        </Card>
+    
+  </div>
+    
+)
+
