9f9a5e89c80472fd6857c486f4a8d5cf776bb11b
Alyson Maruyama <alyson1907@gmail.com>
2020-09-28 22:35:19 -0300

    Added Morgan and Base64 as dependency

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,7 @@ const env = process.env.NODE_ENV || 'development'
 import dotenv from 'dotenv'
 dotenv.config()
 import './initializeDB'
+import morgan from 'morgan'
 import express from 'express'
 import router from './routes'
 import { errorHandler } from './error'
@@ -9,8 +10,9 @@ import { errorHandler } from './error'
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use(morgan('dev'))
 app.use(express.json())
 app.use('/', router)
 app.use(errorHandler)
 
-app.listen(PORT, () => console.log('Server listening on port', PORT))
+app.listen(PORT, () => console.log(`[${env}] - Server listening on port ${PORT}`))
