af635bec12b0e1d2c231482234f02253de8fbd1d
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-10-01 19:58:22 -0300

    dprs

--- frontend/src/components/layout/Card.jsx
+++ frontend/src/components/layout/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import uwuBotton from './uwuButton'
+import owo from '../uwuButton'
+
 import './Card.css'
   
 
 
 
 export default props =>
+     
     
 
     <div className="Card">
@@ -17,17 +19,11 @@ export default props =>
         </div>
         <div className='input-button'>
             <label><input className = "input" type='text' placeholder = "coloque o link aqui"></input></label>
-             {uwuBotton()}
+            {owo()}
+            
         </div>
 
     </div>
 
 
 
-{/* <div className='Card'>
-    <p className='Conteudo'>Encurta Link</p>
-    <div className='input-botao'>
-      <label>URL<input type='text'></input></label>
-      <button>OK!!</button>
-    </div>
-  </div> */}
---
+++ frontend/src/components/uwuButton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+
+
+    function owo(props) {
+
+        function uwuButton(props) {
+          const uwu = 'uwu'
+          console.log(uwu)
+        }
+        
+        return (
+          <button className= "Button" onClick={uwuButton.props}>
+            Click me!
+          </button>
+        );
+      }
+      
+    export default owo
