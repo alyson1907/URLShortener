17f5e63bf5befdb5d03743840cda8811238e8446
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-10-02 09:26:06 -0300

    input log on click

--- frontend/src/components/layout/Card.jsx
+++ frontend/src/components/layout/Card.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
-import owo from '../uwuButton'
+import React, { Component } from 'react'
+import { useState } from 'react'
+
 
 import './Card.css'
   
 
 
 
-export default props =>
-     
+export default (props) => {
     
+    const [grabInput, setNewgrabInput] = useState(null)
+   
+    function  handleChange (event) {
+       setNewgrabInput(event.target.value);
+       
+    }   
+    
+    function handleClick(event){
+        
+        console.log(grabInput)
+        
+    }
 
+    
+    return (
     <div className="Card">
         <p className="Conteudo">
             Encurta Link !!!
@@ -17,13 +31,19 @@ export default props =>
         <div className="Footer">
             {props.titulo}
         </div>
+        
         <div className='input-button'>
-            <label><input className = "input" type='text' placeholder = "coloque o link aqui"></input></label>
-            {owo()}
+           
+            <label><input  className = "input" type='text' placeholder = "coloque o link aqui" onChange={handleChange}></input></label>
+            <button type="button" className= "Button" onClick={handleClick}>
+            Click me!
+            </button>
+            
             
         </div>
 
     </div>
-
-
+    )
+    
+}
 
