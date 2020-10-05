0ca870106178ee7a8ff200e23a4bc1a969d6ac97
Alyson Maruyama <alyson1907@gmail.com>
2020-10-05 20:28:50 -0300

    Added text error logic

--- frontend-html/script.js
+++ frontend-html/script.js
@@ -1,3 +1,17 @@
+// TODO implement url validation
+const isValidURL = (url) => true
 
-document.getElementById('url-input-button').onclick = () => console.log('Clicked!!')
+const handleSubmit = () => {
+  const url = document.getElementById('url-text-input').value
+  const isValid = isValidURL(url)
 
+  if (!isValid) {
+    const errorElem = document.getElementById('text-error')
+    console.log('invalid!')
+    errorElem.classList.remove('hidden')
+    errorElem.classList.add('show')
+  } else {
+  }
+}
+
+document.getElementById('url-input-button').onclick = handleSubmit
