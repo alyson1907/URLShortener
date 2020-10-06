84894c73bfac4552125e9fe0660ef9ef5950e783
Leonardo-Costa-de-Almeida <62718629+Leonardo-Costa-de-Almeida@users.noreply.github.com>
2020-10-06 07:44:22 -0300

    half-done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Card from './components/layout/Card'
+
 import './App.css'
+
 export default (props) => (
     <div className="App">
         <Card titulo = "Insira o Link no Campo!" >
--- frontend/src/components/layout/Card.jsx
+++ frontend/src/components/layout/Card.jsx
@@ -1,27 +1,52 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { useState } from 'react'
+import axios from 'axios'
 
 
 import './Card.css'
-  
+
+
 
 
 
 export default (props) => {
+    
+   
     //event tracka
     const [grabInput, setNewgrabInput] = useState(null)
+    const [uwu , setNewUwu] = useState(null)
     
+   
+   
     function  handleChange (event) {
-       setNewgrabInput(event.target.value);
+        
+        setNewgrabInput(event.target.value);
        
     }   
     
-    function handleClick(event){
+    
+
+
+   async function handleClick(event){
+        	event.preventDefault() 
+      console.log(grabInput)
+      
+      const uwus = await axios.post('https://ppshort.herokuapp.com/shorten', {"url": grabInput})
+      .then ( response => {
+        console.log(response.data.short)
+        setNewUwu(response.data.short)
         
-        console.log(grabInput)
+           
+            
+      } )
+        .catch(error => {
+            console.log(error)
+        })
+        
+        
         
     }
-
+    
     
     return (
     <div className="Card">
@@ -37,8 +62,9 @@ export default (props) => {
             <label><input  className = "input" type='text' placeholder = "coloque o link aqui" onChange={handleChange}></input></label>
             <button type="button" className= "Button" onClick={handleClick}>
             Click me!
-            </button>
             
+            </button>
+            <h1 className="uwu" type = "text">https://ppshort.herokuapp.com/{uwu}</h1>
             
         </div>
 
