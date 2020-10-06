6ff8e7ec4ca5ca35c108fbb65e3483b98636aa1e
Alyson Maruyama <alyson1907@gmail.com>
2020-10-05 22:33:14 -0300

    [WIP] Sending request to Back-End and showing errors

--- frontend-html/script.js
+++ frontend-html/script.js
@@ -1,17 +1,42 @@
 // TODO implement url validation
-const isValidURL = (url) => true
+const isValidURL = (url) => false
 
-const handleSubmit = () => {
+const showErrors = (errors = [], time = 1500) => {
+  const errorElem = document.getElementById('text-error')
+
+  errorElem.innerText = 'A URL é inválida'
+  errorElem.classList.add('show')
+  setTimeout(() => errorElem.classList.remove('show'), 1500)
+}
+
+const handleSubmit = async () => {
   const url = document.getElementById('url-text-input').value
   const isValid = isValidURL(url)
 
   if (!isValid) {
-    const errorElem = document.getElementById('text-error')
-    console.log('invalid!')
-    errorElem.classList.remove('hidden')
-    errorElem.classList.add('show')
+    showErrors(['A URL é inválida'])
   } else {
+    await createShortURL(url)
   }
+}
+
+const createShortURL = async (url) => {
+  const response = await fetch('https://ppshort.herokuapp.com/shorten', {
+    method: 'POST',
+    headers: new Headers({ 'content-type': 'application/json' }),
+    body: JSON.stringify({ url }),
+    mode: 'cors',
+  })
+  const data = await response.json()
+
+  if (!response.ok) {
+    // TODO implement complete ErrorHandler
+    console.log(data)
+    // alert('Error while creating URL')
+    return
+  }
+
+  return data
 }
 
 document.getElementById('url-input-button').onclick = handleSubmit
