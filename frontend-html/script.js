6778b60e9d0db3287cfa6c56db6cefcf36233e65
Alyson Maruyama <alyson1907@gmail.com>
2020-10-06 20:29:16 -0300

    Implements Copy to Clipboard and fetching request

--- frontend-html/script.js
+++ frontend-html/script.js
@@ -1,27 +1,46 @@
+const baseUrl = 'https://ppshort.herokuapp.com'
+
 // TODO implement url validation
-const isValidURL = (url) => false
+const isValidURL = (url) => true
 
 const showErrors = (errors = [], time = 1500) => {
-  const errorElem = document.getElementById('text-error')
-
-  errorElem.innerText = 'A URL é inválida'
+  const errorElem = document.getElementById('errors')
+  errors.forEach((err) => (errorElem.innerHTML += `<p>${err}*<p>`))
   errorElem.classList.add('show')
-  setTimeout(() => errorElem.classList.remove('show'), 1500)
+  setTimeout(() => {
+    errorElem.classList.remove('show')
+    errorElem.innerHTML = ''
+  }, time)
 }
 
-const handleSubmit = async () => {
+const copyToClipboard = (inputElem) => {
+  inputElem.select()
+  inputElem.setSelectionRange(0, 99999) // For mobile devices
+  document.execCommand('copy')
+}
+
+const handleSubmitClick = async () => {
   const url = document.getElementById('url-text-input').value
   const isValid = isValidURL(url)
 
   if (!isValid) {
     showErrors(['A URL é inválida'])
   } else {
-    await createShortURL(url)
+    const { short } = await createShortURL(url)
+    const completeUrl = `${baseUrl}/${short}`
+    // Showing Short-URL
+    const copyUrlContainer = document.getElementById('copy-url-input')
+    copyUrlContainer.value = completeUrl
   }
 }
 
+const handleCopyClick = () => {
+  const elem = document.getElementById('copy-url-input')
+  copyToClipboard(elem)
+}
+
 const createShortURL = async (url) => {
-  const response = await fetch('https://ppshort.herokuapp.com/shorten', {
+  const response = await fetch(baseUrl + '/shorten', {
     method: 'POST',
     headers: new Headers({ 'content-type': 'application/json' }),
     body: JSON.stringify({ url }),
@@ -31,12 +50,12 @@ const createShortURL = async (url) => {
 
   if (!response.ok) {
     // TODO implement complete ErrorHandler
-    console.log(data)
     // alert('Error while creating URL')
     return
   }
-
+  console.log(data)
   return data
 }
 
-document.getElementById('url-input-button').onclick = handleSubmit
+document.getElementById('url-input-button').onclick = handleSubmitClick
+document.getElementById('copy-button').onclick = handleCopyClick
